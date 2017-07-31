3e70321fec3d5d36f3865aa6aacc94214f198a94
avil13 <13.avil@gmail.com>
2017-07-31 11:49:29 +0300

    8 - toast

--- app/src/main/java/com/example/avil/myapplication/MainActivity.java
+++ app/src/main/java/com/example/avil/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,6 +15,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -111,5 +113,12 @@ public class MainActivity extends AppCompatActivity
         intent.putExtra("txt_val", "Work!!!");
 
         startActivity(intent);
+    }
+
+
+    public void alertToast(View v) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Hello world", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
 }

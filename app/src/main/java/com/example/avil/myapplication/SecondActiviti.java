c73e9699ca346d7bc13fcb611468944abb65b915
avil13 <13.avil@gmail.com>
2017-08-02 18:05:37 +0300

    11. AlertDialog

--- app/src/main/java/com/example/avil/myapplication/SecondActiviti.java
+++ app/src/main/java/com/example/avil/myapplication/SecondActiviti.java
@@ -1,11 +1,14 @@
 package com.example.avil.myapplication;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by avil on 28.07.17.
@@ -34,6 +37,35 @@ public class SecondActiviti extends Activity {
         intent.putExtra("text", "Ok man!");
         setResult(RESULT_OK, intent);
         finish();
+    }
+
+
+    public void showDialog(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Самый лучший алерт!!!")
+                .setMessage("Здесь мы тестируем самый современный способ отображения")
+                .setNegativeButton("Нафиг", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(), "Ну нет, так нет. Бывает...", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setPositiveButton("Тру", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(), "Мужественное решение. Уважаю!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialogInterface) {
+                        Toast.makeText(getApplicationContext(), "Ушел? Вот УГ...", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
 }
